4791dad2c52caafcef3cbc1d1841d602c3e7b5c9
ashish <rr222001@gmail.com>
2024-05-25 19:53:55 +0530

    added router configuration for insert api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import express from "express"
 import generateRoutes from './routes/generateRoutes.js'
+import insertRoutes from './routes/insertRoutes.js'
 
 const app = express()
+const rootUrl = "/api/v1"
 app.use(express.json())
-app.use('/api/v1', generateRoutes)
+app.use(rootUrl, generateRoutes)
+app.use(rootUrl, insertRoutes)
 
 export default app
