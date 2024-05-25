35f9d5af3085e3825754936f6a0f81808db6aaf9
ashish <rr222001@gmail.com>
2024-05-25 14:44:59 +0530

    changed fallback due to openai error

--- backend/src/config/llmConfig.js
+++ backend/src/config/llmConfig.js
@@ -1,6 +1,6 @@
 export const url = "https://api.edenai.run/v2/text/generation"
 export const bearerToken = process.env.EDEN_BEARER_TOKEN
 export const providers = "google/gemini-pro"
-export const fallbackProviders = "openai/text-babbage-002"
+export const fallbackProviders = "cohere"
 export const temperature = 0.67
 export const maxTokens = 650
