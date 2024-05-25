0c1c00bd7801c909b8e04fc040d9ac7ee2ec27b2
ashish <rr222001@gmail.com>
2024-05-25 22:20:15 +0530

    added enums for tone and length

---
+++ frontend/src/constants/Options.js
@@ -0,0 +1,11 @@
+export const TONE_OPTIONS = Object.freeze({
+    CASUAL: "Casual",
+    FORMAL: "Formal",
+    GRANDIOSE: "Grandiose"
+})
+
+export const LENGTH_OPTIONS = Object.freeze({
+    SHORT: "4 to 6 sentences",
+    MEDIUM: "8 to 10 sentences",
+    LONG: "15 to 20 sentences"
+})
