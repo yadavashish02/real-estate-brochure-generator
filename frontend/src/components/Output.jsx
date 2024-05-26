ef6e724f3ab079dc6b5d89175bd35213601ed6cb
ashish <rr222001@gmail.com>
2024-05-26 12:24:05 +0530

    added maxWidth to output

--- frontend/src/components/Output.jsx
+++ frontend/src/components/Output.jsx
@@ -1,7 +1,7 @@
 const Output = ({output, setOutput}) => {
     return (
         <>
-        <div style={{border: "2px solid black", marginTop: 10, padding: 5, marginBottom: 10}}>
+        <div style={{border: "2px solid black", marginTop: 10, padding: 5, marginBottom: 10, maxWidth: 600}}>
         <pre>{output}</pre>
         </div>
         </>
