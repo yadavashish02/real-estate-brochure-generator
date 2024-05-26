2daa1b684fcb952c47ba4608413c758dbd26b523
ashish <rr222001@gmail.com>
2024-05-26 15:39:29 +0530

    cors error fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,12 @@ import regenerateRoute from "./routes/regenerateRoutes.js"
 const app = express()
 const rootUrl = "/api/v1"
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    res.header("Access-Control-Allow-Methods","POST")
+    next()
+})
 app.use(rootUrl, generateRoute)
 app.use(rootUrl, insertRoute)
 app.use(rootUrl, regenerateRoute)
