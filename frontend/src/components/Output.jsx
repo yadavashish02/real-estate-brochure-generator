4d1d65a2ccb220444c4ad1b2cba9c3606a7e2a03
ashish <rr222001@gmail.com>
2024-05-26 11:45:28 +0530

    created functional UI

---
+++ frontend/src/components/Output.jsx
@@ -0,0 +1,11 @@
+const Output = ({output, setOutput}) => {
+    return (
+        <>
+        <div style={{border: "2px solid black", marginTop: 10, padding: 5}}>
+        <p>{output}</p>
+        </div>
+        </>
+    )
+}
+
+export default Output
--- frontend/src/components/TextBox.jsx
+++ frontend/src/components/TextBox.jsx
@@ -1,19 +1,22 @@
 import React, {useState} from "react"
 
-const TextInput = ({placeholder, onChange}) => {
+const TextInput = ({id, name, placeholder, onChange}) => {
     const [input, setInput] = useState("")
 
     const handleChange = (event) => {
         setInput(event.target.value)
-        onChange(event.target.value)
+        onChange(event)
     }
 
     return (
         <input
             type="text"
+            id={id}
+            name={name}
             value={input}
             onChange={handleChange}
             placeholder={placeholder}
+            required
         />
     )
 }
